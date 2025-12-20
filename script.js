9eaac1105511fc5ca6e501283e9461b2144edb88
Atanu Rooj <145473546+TheOneWhoCreate@users.noreply.github.com>
2025-12-20 22:22:04 +0530

    opacity = 1

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function createConfetti() {
         confetti.style.backgroundColor =
             selectedPalette[Math.floor(Math.random() * selectedPalette.length)];
         confetti.style.animationDuration = Math.random() * 3 + 2 + "s";
-        confetti.style.opacity = Math.random();
+        confetti.style.opacity = 1;
         confetti.style.borderRadius = Math.random() > 0.5 ? "50%" : "0";
         const size = Math.random() * 6 + 6;
         confetti.style.width = confetti.style.height = size + "px";
